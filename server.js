5f98a3dd6524b47c2fe16bec78accc84dd1d499b
Gaurav Srivastava <gauravjss@gmail.com>
2018-08-13 01:56:21 -0500

    Adding configuration for Heroku

---
+++ server.js
@@ -0,0 +1,32 @@
+//Install express server
+var express = require('express');
+var app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist'));
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080,
+  function(){
+    console.log("Server Started ");
+  });
+
+app.use(function(req, res, next) {
+  res.status(404).redirect('/');
+});
+
+/*app.get('/', function(request, response) {
+  console.error(request);
+  response.render('pages/index')
+});*/
+
+
+/*app.use(function (req, res, next) {
+  res.status(404).render(__dirname + '/dist/index');
+})*/
+
+/*
+app.get('*', function(req, res){
+  res.status(404).render('pages/index');
+});
+*/
